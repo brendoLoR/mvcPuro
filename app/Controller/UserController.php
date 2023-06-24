1881cc5787b78d0ba29af07447690fdd522f9283
Brendo Silva <brendoja@gmail.com>
2023-06-23 23:32:24 -0300

    feat: usando database

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -1,16 +1,20 @@
 <?php
 namespace App\Controller;
-use App\Core\Response;
+use App\Core\Database\DBQuery;
+use app\Core\Http\Response;
 
 class UserController extends BaseController
 {
-    public function index($user_id): \App\Core\Response
+    public function index($user_id): \app\Core\Http\Response
     {
         return Response::getResponse()->status(200)
             ->message('Index')
             ->json([
                 'teste' => 'resultado',
-                'userId' => $user_id
+                'user' => DBQuery::table('users')
+                    ->select(['name', 'email'])
+                    ->where('id', '=', "$user_id")
+                    ->first(),
             ]);
     }
 
