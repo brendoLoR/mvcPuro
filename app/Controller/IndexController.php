76f02fe7b201859d908ea4269d993f472d3f8d2d
Brendo Silva <brendoja@gmail.com>
2023-06-23 23:38:55 -0300

    fix: namespace

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -1,10 +1,11 @@
 <?php
 namespace App\Controller;
-use app\Core\Http\Response;
+
+use App\Core\Http\Response;
 
 class IndexController extends BaseController
 {
-    public function index(): \app\Core\Http\Response
+    public function index(): Response
     {
         return Response::getResponse()->status(200)
             ->message('Index')
