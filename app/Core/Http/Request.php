661b078e56f8f45f4652ee331939e266d5bdc272
Brendo Silva <brendoja@gmail.com>
2023-06-26 23:25:16 -0300

    feat: ad validations rules

--- app/Core/Http/Request.php
+++ app/Core/Http/Request.php
@@ -2,6 +2,8 @@
 
 namespace App\Core\Http;
 
+use App\Core\Database\DBQuery;
+
 /**
  * core general propose Request class
  */
@@ -11,6 +13,7 @@ final class Request
     public string $uri;
     public string $method;
     public mixed $body;
+    public array $errors = [];
 
     private function __construct()
     {
@@ -47,4 +50,56 @@ final class Request
         return [];
     }
 
+    /**
+     * @throws \Exception
+     */
+    public function validate(array $rules): bool|array
+    {
+        $validations = $this->getValidations();
+
+        $validated = [];
+        foreach ($rules as $value => $rules) {
+            if (!is_array($rules)) {
+                $rules = [$rules];
+            }
+            foreach ($rules as $rule) {
+                $rule = explode(":", $rule);
+                if (!isset($validations[$rule[0]])) {
+                    throw new \Exception("validation mismatch");
+                }
+
+                if (!$validations[$rule[0]]($this->body, $value, $rule[1] ?? null)) {
+                    $this->errors[$value] = $this->getMessages()[$rule[0]]($value);
+                    return false;
+                }
+
+            }
+            $validated[$value] = $this->body[$value];
+        }
+        return $validated;
+    }
+
+    public function getErrorsMessages(): bool|array
+    {
+        return empty($this->errors) ? false : $this->errors;
+    }
+
+    private function getValidations(): array
+    {
+        return [
+            'required' => fn($requestBody, $nedded, $attr = null) => isset($requestBody[$nedded]),
+            'unique' => fn($requestBody, $nedded, $attr) => is_null(DBQuery::table($attr)
+                ->select()
+                ->where($nedded, '=', $requestBody[$nedded])
+                ->first()),
+        ];
+    }
+
+    private function getMessages(): array
+    {
+        return [
+            'required' => fn($attribute) => "$attribute is required",
+            'unique' => fn($attribute) => "$attribute must be unique",
+        ];
+    }
 }
