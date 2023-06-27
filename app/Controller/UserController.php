d44c8c2358311cc7bdca009ada24ade3c4817c35
Brendo Silva <brendoja@gmail.com>
2023-06-26 23:25:29 -0300

    feat: ad create user method

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace App\Controller;
+
 use App\Core\Database\DBQuery;
+use App\Core\Http\Request;
 use App\Core\Http\Response;
 
 class UserController extends BaseController
@@ -10,12 +13,41 @@ class UserController extends BaseController
         return Response::getResponse()->status(200)
             ->message('Index')
             ->json([
-                'teste' => 'resultado',
                 'user' => DBQuery::table('users')
                     ->select(['name', 'email'])
                     ->where('id', '=', "$user_id")
                     ->first(),
             ]);
+    }
+
+    public function create(): Response
+    {
+        $request = Request::getRequest();
+        if (!$validated = $request
+            ->validate([
+                'name' => 'required',
+                'email' => ['required', 'unique:users'],
+                'password' => 'required',
+            ])) {
+            return Response::getResponse()->status(400)
+                ->message("Request error")
+                ->json($request->getErrorsMessages());
+        }
+
+        if (!($user = DBQuery::table('users')
+            ->insert($validated))) {
+            return Response::getResponse()->status(501)
+                ->message("Error on server")
+                ->json([
+                    'status' => 501
+                ]);
+        }
+        return Response::getResponse()->status(200)
+            ->message("User created")
+            ->json([
+                'userId' => $user['id'] ?? '',
+            ]);
+
     }
 
 }
