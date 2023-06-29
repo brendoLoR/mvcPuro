621ba1e0aae4c703f4d8bdbe144d9257f593cea3
Brendo Silva <brendoja@gmail.com>
2023-06-28 22:32:08 -0300

    fix: add token to hiddens

--- app/Model/User.php
+++ app/Model/User.php
@@ -2,17 +2,15 @@
 
 namespace App\Model;
 
-use App\Core\Database\DBQuery;
-
 class User extends Model
 {
     protected string $table = 'users';
 
-    protected static array $hidden = ['password'];
+    protected static array $hidden = ['password', 'token'];
 
-    public static function attemptLogin(mixed $email, mixed $password): DBQuery|Model|bool
+    public static function attemptLogin(mixed $email, mixed $password): string|bool
     {
-        $password = hash('sha256', $password);
+        $password = self::getHash($password);
 
         $user = new static();
 
@@ -24,19 +22,20 @@ class User extends Model
         }
 
         $user = new User($user);
-        $token = hash('sha256', $user->getAttribute('email') . microtime());
-        return $user->update(['token' => $token]);
+        $token = self::getHash($user->getAttribute('email') . microtime());
+        return $user->update(['token' => $token]) ? $token : false;
+
     }
 
     public static function checkToken(string $token)
     {
-        if(strlen($token) < 64){
+        if (strlen($token) < 64) {
             return false;
         }
 
-        if(!$data = (new static())->query()
+        if (!$data = (new static())->query()
             ->where('token', '=', $token)
-            ->first()){
+            ->first()) {
             return false;
         }
 
@@ -46,9 +45,29 @@ class User extends Model
     protected static function beforeSave(Model $model): Model
     {
         if ($password = $model->getAttribute('password')) {
-            $model->setAttribute('password', hash('sha256', $password));
+            $model->setAttribute('password', self::getHash($password));
         }
         return $model;
+    }
+
+    protected static function beforeUpdate(array $attributes): array
+    {
+        if (empty($attributes['password'])) {
+            unset($attributes['password']);
+            return $attributes;
+        }
+
+        $attributes['password'] = User::getHash($attributes['password']);
+        return $attributes;
+    }
+
+    /**
+     * @param mixed $password
+     * @return string
+     */
+    public static function getHash(mixed $password): string
+    {
+        return hash('sha256', $password);
     }
 
 }
