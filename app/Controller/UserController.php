a4b85e4179318bb9ca74afd7d6028a0e7a764415
Brendo Silva <brendoja@gmail.com>
2023-06-28 22:32:22 -0300

    fix: update method

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Core\Database\DBQuery;
-use App\Core\Http\Request;
 use App\Core\Http\Response;
 use App\Model\User;
 
@@ -20,7 +18,7 @@ class UserController extends BaseController
 
     public function show($user_id): Response
     {
-        
+
         return $this->response()->status(200)
             ->message('Index')
             ->json([
@@ -33,19 +31,17 @@ class UserController extends BaseController
      */
     public function create(): Response
     {
-        $request = Request::getRequest();
-        if (!$validated = $request->validate([
-                'name' => 'required',
-                'email' => ['required', 'email', 'unique:users',],
-                'password' => 'required',
-            ])) {
-            return $this->response()->status(400)->message("Request error")->json($request->getErrorsMessages());
+
+        if (!$validated = $this->request()->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users',],
+            'password' => 'required',
+        ])) {
+            return $this->abort(400, "Request error");
         }
 
-        if (!($user = (new User($validated))->save())) {
-            return $this->response()->status(501)->message("Error on server")->json([
-                'status' => 501
-            ]);
+        if (!($user = User::create($validated))) {
+            return $this->abort(501, "Error on server");
         }
 
         return $this->response()->status(200)->message("User created")->json([
@@ -53,9 +49,42 @@ class UserController extends BaseController
         ]);
     }
 
+    /**
+     * @throws \Exception
+     */
+    public function update($user_id): Response
+    {
+        if ($this->request()->user()->getAttribute('id') != $user_id) {
+            return $this->abort();
+
+        }
+        if (!$validated = $this->request()->validate([
+            'name' => 'required',
+            'email' => ['required', 'email', 'unique:users,' . $user_id],
+            'password' => ['nullable'],
+        ])) {
+            return $this->abort(400, "Request error");
+        }
+
+        if(!$user = User::find($user_id, filter: false)){
+            return $this->abort(404, "User not found");
+        }
+
+        if (!$user->update($validated)) {
+            return $this->abort(501, "Error on server");
+        }
+
+        return $this->response()->status(200)->message("User updated")->json([
+            'userId' => $user->getAttribute('id') ?? '',
+        ]);
+    }
+
 
     public function delete($user_id): Response
     {
+        if ($this->request()->user()->getAttribute('id') != $user_id) {
+            return $this->abort();
+        }
         return $this->response()->status(200)
             ->message('Index')
             ->json([
