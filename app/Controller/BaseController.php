b38dc68c558b78d967cd9aefb0b85616bfe46292
Brendo Silva <brendoja@gmail.com>
2023-06-28 22:29:25 -0300

    feat: add request e helper para abortar a request

--- app/Controller/BaseController.php
+++ app/Controller/BaseController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Core\Http\Request;
 use App\Core\Http\Response;
 
 class BaseController
@@ -8,5 +9,21 @@ class BaseController
     protected function response(): Response
     {
         return Response::getResponse();
+    }
+    protected function request(): Request
+    {
+        return Request::getRequest();
+    }
+
+    /**
+     * @param int|null $status
+     * @param string|null $message
+     * @return Response
+     */
+    protected function abort(?int $status = 403, ?string $message = "Not authorized"): Response
+    {
+        return $this->response()->status($status)
+            ->message($message)
+            ->json(['errors' => $this->request()->getErrorsMessages()]);
     }
 }
