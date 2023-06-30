c92cb0a4d2fc413ebd76493417ba295d0c1031b8
Brendo Silva <brendoja@gmail.com>
2023-06-29 21:02:35 -0300

    feat: wrap auth metodos em uma trait

--- app/Model/User.php
+++ app/Model/User.php
@@ -2,72 +2,13 @@
 
 namespace App\Model;
 
+use App\Traits\HasAuthentication;
+
 class User extends Model
 {
+    use HasAuthentication;
     protected string $table = 'users';
 
     protected static array $hidden = ['password', 'token'];
-
-    public static function attemptLogin(mixed $email, mixed $password): string|bool
-    {
-        $password = self::getHash($password);
-
-        $user = new static();
-
-        if (!$user = $user->query()
-            ->where('email', '=', $email)
-            ->where('password', '=', $password)
-            ->first()) {
-            return false;
-        }
-
-        $user = new User($user);
-        $token = self::getHash($user->getAttribute('email') . microtime());
-        return $user->update(['token' => $token]) ? $token : false;
-
-    }
-
-    public static function checkToken(string $token)
-    {
-        if (strlen($token) < 64) {
-            return false;
-        }
-
-        if (!$data = (new static())->query()
-            ->where('token', '=', $token)
-            ->first()) {
-            return false;
-        }
-
-        return new static($data);
-    }
-
-    protected static function beforeSave(Model $model): Model
-    {
-        if ($password = $model->getAttribute('password')) {
-            $model->setAttribute('password', self::getHash($password));
-        }
-        return $model;
-    }
-
-    protected static function beforeUpdate(array $attributes): array
-    {
-        if (empty($attributes['password'])) {
-            unset($attributes['password']);
-            return $attributes;
-        }
-
-        $attributes['password'] = User::getHash($attributes['password']);
-        return $attributes;
-    }
-
-    /**
-     * @param mixed $password
-     * @return string
-     */
-    public static function getHash(mixed $password): string
-    {
-        return hash('sha256', $password);
-    }
 
 }
---
+++ app/Traits/HasAuthentication.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Traits;
+
+use App\Model\Model;
+use App\Model\User;
+
+trait HasAuthentication
+{
+    public static function attemptLogin(mixed $email, mixed $password): string|bool
+    {
+        $password = self::getHash($password);
+
+        $user = new static();
+
+        /** @var User $user */
+        if (!$userData = $user->query()
+            ->where('email', '=', $email)
+            ->where('password', '=', $password)
+            ->first()) {
+            return false;
+        }
+
+        $user = new User($userData);
+        $token = self::getHash($user->getAttribute('email') . microtime());
+        return $user->update(['token' => $token]) ? $token : false;
+
+    }
+
+    public static function checkToken(string $token): bool|static
+    {
+        if (strlen($token) < 64) {
+            return false;
+        }
+
+        if (!$data = (new static())->query()
+            ->where('token', '=', $token)
+            ->first()) {
+            return false;
+        }
+
+        return new static($data);
+    }
+
+    protected static function beforeSave(Model $model): Model
+    {
+        if ($password = $model->getAttribute('password')) {
+            $model->setAttribute('password', self::getHash($password));
+        }
+        return $model;
+    }
+
+    protected static function beforeUpdate(array $attributes): array
+    {
+        if (empty($attributes['password'])) {
+            unset($attributes['password']);
+            return $attributes;
+        }
+
+        $attributes['password'] = self::getHash($attributes['password']);
+        return $attributes;
+    }
+
+    /**
+     * @param mixed $password
+     * @return string
+     */
+    public static function getHash(mixed $password): string
+    {
+        return hash('sha256', $password);
+    }
+}
