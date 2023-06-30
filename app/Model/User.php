22fb8683ac5ef895438276b05c7b9bd00f3ea280
Brendo Silva <brendoja@gmail.com>
2023-06-29 23:19:51 -0300

    feat: metodos para trazer os dados das bebidas por usuário

--- app/Model/User.php
+++ app/Model/User.php
@@ -7,8 +7,32 @@ use App\Traits\HasAuthentication;
 class User extends Model
 {
     use HasAuthentication;
+
     protected string $table = 'users';
 
     protected static array $hidden = ['password', 'token'];
 
+    protected Drink $drink;
+
+    public function __construct(?array $attributes = null)
+    {
+        parent::__construct($attributes);
+        $this->drink = new Drink();
+    }
+
+    public function drinks(): \App\Core\Database\DBQuery
+    {
+        return $this->drink
+            ->where('user_id', '=', $this->getAttribute('id'))
+            ->select();
+    }
+
+    protected function getWithCountQuery(): null|string
+    {
+        return "(". $this->drink
+            ->where('drinks.user_id', '=', '`users`.`id`')
+            ->group(['drinks.user_id'])
+            ->select(['sum(drink)'])
+            ->query(true) . ") as drinkCounter" ;
+    }
 }
