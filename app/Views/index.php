9d820b93830e7265ea1805504811f2579ceadcf1
Brendo Silva <brendoja@gmail.com>
2023-07-01 16:57:23 -0300

    fix: remove mock

--- app/Views/index.php
+++ app/Views/index.php
@@ -18,7 +18,7 @@
 
     <div class="row justify-content-center">
         <div class="col-md-8" id="login-container">
-            <div class="row justify-content-center" >
+            <div class="row justify-content-center">
                 <div class="col-md-6">
                     <div class="card">
                         <div class="card-header">
@@ -29,12 +29,11 @@
                                 <div class="mb-3">
                                     <label for="exampleInputEmail1" class="form-label">Email</label>
                                     <input type="email" class="form-control" name="email" id="login-email"
-                                           aria-describedby="emailHelp" value="brendoja@hotmail.com">
+                                           aria-describedby="emailHelp">
                                 </div>
                                 <div class="mb-3">
                                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                                    <input type="password" class="form-control" name="password" id="login-password"
-                                           value="password">
+                                    <input type="password" class="form-control" name="password" id="login-password">
                                 </div>
 
                                 <button type="submit" class="btn btn-primary">Submit</button>
